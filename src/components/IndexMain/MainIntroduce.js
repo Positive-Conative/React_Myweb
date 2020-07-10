b1687baca4aafac6a02bd442ff58056e046a4bac
kyj970513 <kyj970513@naver.com>
2020-07-10 11:51:28 +0900

    v

--- src/components/IndexMain/MainIntroduce.js
+++ src/components/IndexMain/MainIntroduce.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import './MainIntroduce.css';
-
+import {Card} from 'react-bootstrap';
 class MainIntroduce extends React.Component{
     render(){
         return(
             <div className="secondSession">
                 <div className="secondSessionHeader">
-                    --- Positive Conative --- 
+                    --- Positive Conative ---
+                </div>
+                <div className="cardBox">dd
+                    <Card className="cards">
+                        <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="cards">
+                        <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="cards">
+                        <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="cards">
+                        <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                            <Card.Title>Dark Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>                    
                 </div>
             </div>
         );
